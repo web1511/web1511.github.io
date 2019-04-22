a731d519874cefec7a73871536763170077b284f
web1511 <2564203980@qq.com>
2019-04-22 10:48:49 +0800

    js例子模板

---
+++ js_gj/js/config.js
@@ -0,0 +1,20 @@
+var listObj = [
+     {
+         title:'玩转属性操作',
+         url : ['揉捏div']
+     },
+     {
+
+        title:'for循环',
+        url : ['乘法表','图片拼图-无js版','数据合并']
+    }
+];
+
+
+function $(obj) {
+   return document.querySelector(obj);
+}
+
+function $$(obj) {
+    return document.querySelectorAll(obj);
+}
