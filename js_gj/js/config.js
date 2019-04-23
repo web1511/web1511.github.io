ffe080da2f9944ad47c6540272787f8dab4bc83c
web1511 <2564203980@qq.com>
2019-04-23 10:13:38 +0800

    下拉菜单

--- js_gj/js/config.js
+++ js_gj/js/config.js
@@ -29,6 +29,10 @@ var listObj = [
             {
                 type: 'hp',
                 t: '数据合并'
+            },
+            {
+                type: 'xl',
+                t: '下拉菜单'
             }
         ]
     }
@@ -43,8 +47,18 @@ function $$(obj) {
     return document.querySelectorAll(obj);
 }
 
+/*** 设置样式 ***/
+
+function css( obj,sVal ) {
+    if( typeof sVal == 'object' &&  sVal ) {
+        for( var i in sVal ) {
+            obj.style[i] = sVal[i];
+        }
+    }
+    return 0;
+}
 //解析地址参数
-function getQueryObject(url) {
+function getQueryObject( url ) {
     url = url == null ? window.location.href : url;
     var search = url.substring(url.lastIndexOf("?") + 1);
     var obj = {};
